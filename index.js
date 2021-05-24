f4fd12be94c97d980c62cfe5ea1ad3833c328eff
RichardEstime <richardjestime@yahoo.com>
2021-05-24 12:21:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+
+  if (feet <= 400) {
+    return 'This one is on me!'
+} else if (feet >2500) {
+    return 'No can do.' 
+} else if (feet > 2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function ternaryCheckCity(destination){
+return destination === "NYC" ? "Okay, sounds good." : "Pittsburgh" ? "No go."
